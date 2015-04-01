4de496774b4dc4e9a8dcd028e8c1f4f74642575d
James Mazer <james.mazer@yale.edu>
2015-04-01 18:15:12 -0400

    added db functions for pseudo-introspection

---
+++ bootstrap_tools.py
@@ -0,0 +1,22 @@
+# -*- Mode: Python; tab-width: 4; py-indent-offset: 4; -*-
+
+def glyph(name):
+    return """<span class="glyphicon glyphicon-%s" aria-hidden="true"></span> """ % name
+
+def check(bool):
+    if bool:
+        return glyph('check')
+    else:
+        return glyph('unchecked')
+
+def today(n=0):
+    import datetime
+    return (datetime.datetime.now() -
+            datetime.timedelta(days=n)).strftime("%Y-%m-%d")
+
+def safeint(x):
+    try:
+        return int(round(x))
+    except TypeError:
+        return 'ND'
+
